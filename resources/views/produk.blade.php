37ac32241db2ce6d175d8eb1bdc513043d0a35ff
unknown <eghaanggara02@gmail.com>
2023-11-16 11:07:33 +0700

    ini dia

--- resources/views/produk.blade.php
+++ resources/views/produk.blade.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Home</title>
+  <title>Halaman Produk</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
@@ -37,27 +37,24 @@
           <td colspan="4"><h3></h3></td>
         </tr>
       </table>
+      
       <div class="container-fluid">
-        <a class="navbar-brand" href="#"><h3> Hamalam Home</h3></a>
+        <a class="navbar-brand" href="#"><h3> Halaman Produk</h3></a>
+        <input class="btn btn-primary" type="button" value="Tambah Produk">
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExampleXxl" aria-controls="navbarsExampleXxl" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
+          
         </button>
+        
+        
   
         <div class="collapse navbar-collapse" id="navbarsExampleXxl">
           <ul class="navbar-nav me-auto mb-2 mb-xl-0">
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Home</a>
+              <a class="nav-link active" aria-current="page" href="/index">Home</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="/produk">Produk</a>
-          
-            </li>
-              <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">Action</a></li>
-                <li><a class="dropdown-item" href="#">Another action</a></li>
-                <li><a class="dropdown-item" href="#">Something else here</a></li>
-              </ul>
-            </li>
           </ul>
           <form role="search">
             <input class="form-control" type="search" placeholder="Search" aria-label="Search">
@@ -65,16 +62,28 @@
         </div>
       </div>
     </nav>
-
-
-
-
-
-
-
-
-
-
+  <table class="table table-dafault table-sm table-hover">
+    <thead>
+      <tr>
+        <th>Kode Produk</th>
+        <th>Nama Produk</th>
+        <th>Jenis Produk</th>
+        <th>Harga</th>
+      </tr>
+    </thead>
+    </tbody>
+      <tr>  
+        <td>BRG001</td>
+        <td>Pena</td>
+        <td>Alat tulis</td>
+        <td>Rp. 20.000</td>
+      </tr>
+      <tr>  
+        <td>BRG002</td>
+        <td>Buku</td>
+        <td>Alat tulis</td>
+        <td>Rp. 15.000</td>
+      </tr>
 
 
 
