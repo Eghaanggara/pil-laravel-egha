9d03dc78339b6a508526140f5340a4a110709005
unknown <eghaanggara02@gmail.com>
2023-12-29 01:07:06 +0700

    Membuat (Create) dan menampilkan Alert pada saat create kedalamdatabase Mysql

--- resources/views/mahasiswa/create.blade.php
+++ resources/views/mahasiswa/create.blade.php
@@ -4,12 +4,93 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Mahasiswa</title>
+  <title>Halaman Tambah Mahasiswa</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+  <style>
+    body {
+      background-color: #f8f9fa;
+      font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    }
+
+    .navbar {
+      background-color: #343a40;
+      box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    }
+
+    .navbar-brand {
+      color: #ffffff;
+      font-weight: bold;
+      font-size: 24px;
+    }
+
+    .navbar-toggler-icon {
+      background-color: #ffffff;
+    }
+
+    .navbar-nav .nav-link {
+      color: #ffffff;
+    }
+
+    .navbar-nav .nav-link:hover {
+      color: #f8f9fa;
+    }
+
+    h1 {
+      color: #343a40;
+      margin-top: 20px;
+    }
+
+    .form-control {
+      margin-bottom: 10px;
+    }
+
+    .btn-primary {
+      background-color: #007bff;
+      border-color: #007bff;
+    }
+
+    .btn-secondary {
+      background-color: #6c757d;
+      border-color: #6c757d;
+    }
+
+    .btn-primary,
+    .btn-secondary {
+      width: 100%;
+    }
+
+    @media (max-width: 576px) {
+      .navbar-brand {
+        margin-right: 0;
+      }
+
+      .navbar-toggler-icon {
+        background-color: #ffffff;
+      }
+
+      .navbar-collapse {
+        margin-top: 10px;
+      }
+
+      .container {
+        margin-top: 20px;
+      }
+
+      .col-sm-4,
+      .col-sm-8 {
+        margin-bottom: 20px;
+      }
+
+      .row mt-4 {
+        margin-top: 20px;
+      }
+    }
+  </style>
 </head>
 <body>
 
-  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <nav class="navbar navbar-expand-lg">
     <div class="container-fluid">
       <a class="navbar-brand" href="/">Laravel</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -58,18 +139,18 @@
           @csrf
           <div class="row">
             <div class="col-sm-4">
-              <label for="">NPM</label>
+              <label for="npm" class="form-label">NPM</label>
               <input type="number" name="npm" class="form-control" placeholder="Input NPM" value="{{ Session::get('npm') }}">
             </div>
             <div class="col-sm-4">
-              <label for="">Nama Mahasiswa</label>
+              <label for="nama_mahasiswa" class="form-label">Nama Mahasiswa</label>
               <input type="text" name="nama_mahasiswa" class="form-control" placeholder="Input Nama Mahasiswa" value="{{ Session::get('nama_mahasiswa') }}">
             </div>
             <div class="col-sm-4">
-              <label for="">Jenis Kelamin</label>
-              <select name="jk" id="" class="form-select">
-                <option>L</option>
-                <option>P</option>
+              <label for="jk" class="form-label">Jenis Kelamin</label>
+              <select name="jk" id="jk" class="form-select">
+                <option value="L" @if(Session::get('jk') == 'L') selected @endif>L</option>
+                <option value="P" @if(Session::get('jk') == 'P') selected @endif>P</option>
               </select>
             </div>
           </div>
@@ -77,22 +158,22 @@
             <div class="col-sm-8">
               <div class="row">
                 <div class="col-sm-5">
-                  <label for="">Tanggal Lahir</label>
+                  <label for="tgl_lahir" class="form-label">Tanggal Lahir</label>
                   <input type="date" name="tgl_lahir" class="form-control" value="{{ Session::get('tgl_lahir') }}">
                 </div>
                 <div class="col-sm-7">
-                  <label for="">Alamat</label>
-                  <input type="text" name="alamat" id="" class="form-control" placeholder="Input alamat" value="{{ Session::get('alamat') }}">
+                  <label for="alamat" class="form-label">Alamat</label>
+                  <input type="text" name="alamat" id="alamat" class="form-control" placeholder="Input alamat" value="{{ Session::get('alamat') }}">
                 </div>
               </div>
             </div>
             <div class="col-sm-4">
               <div class="row mt-4">
                 <div class="col-sm-6">
-                  <button class="btn btn-primary" style="width: 100%" type="submit">Simpan</button>
+                  <button class="btn btn-primary" type="submit">Simpan</button>
                  </div>
                  <div class="col-sm-6">
-                   <a href="/mahasiswa" class="btn btn-secondary" style="width: 100%">Kembali</a>
+                   <a href="/mahasiswa" class="btn btn-secondary">Kembali</a>
                  </div>
               </div>
             </div>
--- resources/views/mahasiswa/index.blade.php
+++ resources/views/mahasiswa/index.blade.php
@@ -6,10 +6,120 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Mahasiswa</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+  <style>
+    body {
+      background-color: #f8f9fa;
+      font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    }
+
+    .navbar {
+      background-color: #343a40;
+      box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    }
+
+    .navbar-brand {
+      color: #ffffff;
+      font-weight: bold;
+      font-size: 24px;
+    }
+
+    .navbar-toggler-icon {
+      background-color: #ffffff;
+    }
+
+    .navbar-nav .nav-link {
+      color: #ffffff;
+    }
+
+    .navbar-nav .nav-link:hover {
+      color: #f8f9fa;
+    }
+
+    h1 {
+      color: #343a40;
+      margin-top: 20px;
+    }
+
+    .btn-info {
+      background-color: #17a2b8;
+      border-color: #17a2b8;
+    }
+
+    .table {
+      margin-top: 20px;
+      box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+      border-radius: 8px;
+      overflow: hidden;
+    }
+
+    .table thead th {
+      background-color: #343a40;
+      color: #ffffff;
+    }
+
+    .table tbody tr:nth-child(odd) {
+      background-color: #ffffff;
+    }
+
+    .table tbody tr:hover {
+      background-color: #f8f9fa;
+    }
+
+    .alert {
+      background-color: #28a745;
+      color: #ffffff;
+      animation: fadeInUp 0.5s ease-out;
+    }
+
+    @keyframes fadeInUp {
+      from {
+        opacity: 0;
+        transform: translateY(20px);
+      }
+      to {
+        opacity: 1;
+        transform: translateY(0);
+      }
+    }
+
+    @media (max-width: 576px) {
+      .navbar-brand {
+        margin-right: 0;
+      }
+
+      .navbar-toggler-icon {
+        background-color: #ffffff;
+      }
+
+      .navbar-collapse {
+        margin-top: 10px;
+      }
+
+      .container {
+        margin-top: 20px;
+      }
+
+      .col-sm-6,
+      .col-sm-12 {
+        margin-bottom: 20px;
+      }
+
+      footer {
+      position: absolute;
+      bottom: 0;
+      width: 100%;
+      background-color: #343a40;
+      color: #ffffff;
+      text-align: center;
+      padding: 1rem;
+    }
+    }
+  </style>
 </head>
 <body>
 
-  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <nav class="navbar navbar-expand-lg">
     <div class="container-fluid">
       <a class="navbar-brand" href="/">Laravel</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -40,54 +150,61 @@
 
     <div class="row">
       <div class="col-sm-6">
-        <h4>Tabel Mahasiswa</h4>
+        <h4 style="margin-bottom: 20px;">Tabel Mahasiswa</h4>
       </div>
       <div class="col-sm-6" style="text-align: right">
-        <a href="/mahasiswa/create" class="btn btn-info btn-sm">Tambah Mahasiswa</a>        
+        <a href="/mahasiswa/create" class="btn btn-info btn-sm">Tambah Mahasiswa</a>
       </div>
     </div>
 
     <div class="row">
       <div class="col-sm-12">
         @if (Session::has('success'))
-            <div class="alert alert-success alert-dismissible fade show" role="alert">
-              <strong>Successfully</strong> {{ Session::get('success') }}.
-              <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-            </div>
+          <div class="alert alert-success alert-dismissible fade show" role="alert">
+            <strong>Successfully</strong> {{ Session::get('success') }}.
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+          </div>
         @endif
       </div>
     </div>
 
     <div class="row">
       <div class="col-sm-12">
-        <table class="table table-danger table-sm table-hover table-striped table-bordered text-center">
-          <thead>
-            <tr>
-              <th>No</th>
-              <th>NPM</th>
-              <th>Nama Mahasiswa</th>
-              <th>Jenis Kelamin</th>
-              <th>Tanggal Lahir</th>
-              <th>Alamat</th>
-            </tr>
-          </thead>
-          <tbody>
-          @foreach ($mahasiswa as $m)
-            <tr>
-              <td>{{ $no++ }}</td>
-              <td>{{ $m->npm }}</td>
-              <td>{{ $m->nama_mahasiswa }}</td>
-              <td>{{ $m->jk }}</td>
-              <td>{{ $m->tgl_lahir }}</td>
-              <td>{{ $m->alamat }}</td>
-            </tr>
-          @endforeach
-          </tbody>
-        </table>
+        <div class="table-responsive">
+          <table class="table table-sm table-hover table-striped table-bordered text-center">
+            <thead>
+              <tr>
+                <th>No</th>
+                <th>NPM</th>
+                <th>Nama Mahasiswa</th>
+                <th>Jenis Kelamin</th>
+                <th>Tanggal Lahir</th>
+                <th>Alamat</th>
+              </tr>
+            </thead>
+            <tbody>
+              @foreach ($mahasiswa as $m)
+                <tr>
+                  <td>{{ $no++ }}</td>
+                  <td>{{ $m->npm }}</td>
+                  <td>{{ $m->nama_mahasiswa }}</td>
+                  <td>{{ $m->jk }}</td>
+                  <td>{{ $m->tgl_lahir }}</td>
+                  <td>{{ $m->alamat }}</td>
+                </tr>
+              @endforeach
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
-
   </div>
+
+  <footer class="bg-dark text-light text-center py-3">
+    <div class="container">
+      <p>@eghaaas 2023 PIL-Laravel &copy;</p>
+    </div>
+  </footer>
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
